1add6d173c689b60982ed9ad0ccd290e30a81195
lemonancy <19722847+lemonancy@users.noreply.github.com>
2025-12-13 14:46:50 -0800

    Update battle_util.c

--- src/battle_util.c
+++ src/battle_util.c
@@ -6387,10 +6387,9 @@ u32 AbilityBattleEffects(u32 caseID, u32 battler, u32 ability, u32 special, u32 
         RecordAbilityBattle(battler, gLastUsedAbility);
     if (effect && caseID <= ABILITYEFFECT_MOVE_END)
         gBattlerAbility = battler;
-
     return effect;
+	}
 }
-
 bool32 TryPrimalReversion(u32 battler)
 {
     if (GetBattlerHoldEffect(battler, FALSE) == HOLD_EFFECT_PRIMAL_ORB
